9c89a5a95b96238bb962deba276f5ab9ff5ba089
heinop <pekka.heino@unikie.com>
2020-09-16 11:34:48 +0300

    step5

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json());
+
+const maxId = 10000000000;
 
 let persons = [
   {
@@ -25,6 +28,10 @@ let persons = [
   }
 ];
 
+const generateId = () => {
+  return Math.floor(Math.random() * maxId);
+}
+
 app.get('/info', (req,res) => {
   res.send(`<div><p>Phonebook has info for ${persons.length} people</p>${new Date()}</div>`)
 });
@@ -48,6 +55,25 @@ app.delete('/api/persons/:id', (req, res) => {
   const id = Number(req.params.id);
   persons = persons.filter(p => p.id !== id);
   res.status(204).end();
+});
+
+app.post('/api/persons', (req, res) => {
+  const body = req.body;
+
+  if (!(body.name && body.number)) {
+    return res.status(400).json({
+      error: 'must have name and number'
+    });
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId()
+  };
+
+  persons = persons.concat(person);
+  res.json(person);
 });
 
 const PORT = 3001;
