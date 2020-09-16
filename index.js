e2c7dd5c3920b715ea9e97960eac97c8409cdd1a
heinop <pekka.heino@unikie.com>
2020-09-16 13:43:51 +0300

    step8

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ const morgan = require('morgan');
 const app = express();
 
 app.use(express.json());
-app.use(morgan('tiny'));
+
+morgan.token('req-body', (req) => {
+  return req.method === 'POST' ? JSON.stringify(req.body) : '';
+});
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :req-body'));
 
 const maxId = 10000000000;
 
