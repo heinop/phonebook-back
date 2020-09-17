8a3cca891c176c8436d66a225b7dd9332f338e6e
heinop <pekka.heino@unikie.com>
2020-09-17 14:18:43 +0300

    task 3.15

--- index.js
+++ index.js
@@ -62,9 +62,14 @@ app.get('/api/persons/:id', (req, res) => {
 });
 
 app.delete('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id);
-  persons = persons.filter(p => p.id !== id);
-  res.status(204).end();
+  Person.findByIdAndRemove(req.params.id)
+    .then(result => {
+      res.status(204).end();
+    })
+    .catch(error => {
+      console.error(error);
+      res.status(500).send({ error: error.message });
+    });
 });
 
 app.post('/api/persons', (req, res) => {
