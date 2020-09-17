e580ef5ebbd7180998711950f41eb7c60bec9440
heinop <pekka.heino@unikie.com>
2020-09-17 15:16:28 +0300

    Part 3.18 Fixed rest of the endpoints

--- index.js
+++ index.js
@@ -16,39 +16,20 @@ morgan.token('req-body', (req) => {
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :req-body'));
 
-const maxId = 10000000000;
-
-let persons = [
-  {
-    name: 'Arto Hellas',
-    number: '040-123456',
-    id: 1
-  },
-  {
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-    id: 2
-  },
-  {
-    name: 'Dan Abramov',
-    number: '12-43-234345',
-    id: 3
-  },
-  {
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122',
-    id: 4
-  }
-];
-
-app.get('/info', (req,res) => {
-  res.send(`<div><p>Phonebook has info for ${persons.length} people</p>${new Date()}</div>`)
+app.get('/info', (req, res, next) => {
+  Person.count()
+    .then(amount => {
+      res.send(`<div><p>Phonebook has info for ${amount} people</p>${new Date()}</div>`)
+    })
+    .catch(error => next(error));
 });
 
-app.get('/api/persons', (req, res) => {
-  Person.find({}).then(persons => {
-    res.json(persons);
-  });
+app.get('/api/persons', (req, res, next) => {
+  Person.find({})
+    .then(persons => {
+      res.json(persons);
+    })
+    .catch(error => next(error));
 });
 
 app.get('/api/persons/:id', (req, res, next) => {
