9f55e14a2920d6263fae6b5e707af7906e6a1e14
heinop <pekka.heino@unikie.com>
2020-09-17 16:13:17 +0300

    task 3.19 Use unique-validator

--- index.js
+++ index.js
@@ -87,6 +87,8 @@ const errorHandler = (error, req, res, next) => {
   console.error(error);
   if (error.name === 'CastError') {
     return res.status(400).send({ error: 'malformatted id' });
+  } else if (error.name === 'ValidationError') {
+    return res.status(400).send({ error: error.message});
   }
   next(error);
 }
